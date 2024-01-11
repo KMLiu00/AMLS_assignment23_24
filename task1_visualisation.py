a0e037cacde8b78400b7fb025563b9c40fde6601
KMLiu00 <zceekl0@ucl.ac.uk>
2024-01-11 18:46:55 +0000

    add visualization for task1 training dataset

--- task1_visualisation.py
+++ task1_visualisation.py
@@ -1,18 +1,24 @@
-
-from medmnist import PneumoniaMNIST
+# import Math Tools
 import matplotlib.pyplot as plt
 import numpy as np
 
+# import PneumoniaMNIST
+from medmnist import PneumoniaMNIST
 
-pneumonia_data = PneumoniaMNIST(split='train', download=True)
 
+# load train dataset
+train_dataset = PneumoniaMNIST(split='train', download=True)
+val_dataset = PneumoniaMNIST(split='val', download=True)
+test_dataset = PneumoniaMNIST(split='test', download=True)
 
-images, labels = pneumonia_data.imgs, pneumonia_data.labels
+# load train dataset images and labels
+images, labels = train_dataset.imgs, train_dataset.labels
+
 
 labels = labels.flatten()
 
 
-normal_images = images[labels == 0][:10]
+normal_images = images[labels == 0][:10] #
 pneumonia_images = images[labels == 1][:10]
 
 
@@ -30,8 +36,8 @@ for i in range(10):
 plt.show()
 
 
-print(len(pneumonia_data))
-sample_image, sample_label = pneumonia_data[0]
+print(len(train_dataset))
+sample_image, sample_label = train_dataset[0]
 sample_image = np.array(sample_image)
 print("Sample label:", sample_label)
 print("Shape of sample image:", sample_image.shape)
